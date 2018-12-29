3c0f0e85f9e17c35ab195aaad920ad783545e9ec
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-12-29 18:45:27 +0800

    MOD:添加适配器模式demo

---
+++ Objective-C/AdapterPattern/AdapterPattern/Demo01/客户类/DYSCourse.h
@@ -0,0 +1,32 @@
+//
+//  DYSCourse.h
+//  AdapterPattern
+//
+//  Created by 丁玉松 on 2018/12/29.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DYSAdapterProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DYSCourse : NSObject
+
+
+/**
+ 添加课程成员
+
+ @param stuff stuff description
+ */
+- (void)addStaff:(id<DYSAdapterProtocol>)stuff;
+
+
+/**
+ 一一自我介绍
+ */
+- (void)introduceSelfOneByOne;
+
+@end
+
+NS_ASSUME_NONNULL_END
